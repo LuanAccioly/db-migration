04d1adee4e9e4acc5000e82ba8cbf8f525a18992
Luan Accioly <acciolyluan@gmail.com>
2024-11-28 12:30:22 -0300

    feat: atualiza por dias

--- app/db/migrate.py
+++ app/db/migrate.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timedelta
+import json
 import gc
 import logging
 import pandas as pd
 from db.postgres.utils import postgres_check_table_columns
+from db.postgres.config import get_postgres_connection, get_postgres_engine_string_url
 from db.sqlserver.utils import sqlserver_check_table_columns
+from db.sqlserver.config import get_connection, get_connection_string_url
 from logs.log_config import setup_logging
 
 setup_logging()
@@ -73,3 +76,115 @@ def migrate_data(sql_conn, postgres_conn, table_name, date_filter, date_column_n
 
 def update_recent_data(sql_conn, postgres_conn, table_name, date_column_name, days):
     filter_date = (datetime.now() - timedelta(days=days)).strftime("%Y%m%d")
+    try:
+        query = f"SELECT * FROM sankhya.{table_name} WHERE {date_column_name} >= '{filter_date}'"
+        df = pd.read_sql(query, sql_conn)
+        df.columns = df.columns.str.lower()
+        logger.info(
+            f"Encontrados {len(df)} registros no SQL Server para sincronização."
+        )
+
+        # Deletar os dados existentes no PostgreSQL para o mesmo intervalo de tempo
+        delete_query = f"""
+        DELETE FROM raw_sankhya.{table_name}
+        WHERE {date_column_name} >= '{filter_date}';
+        """
+        with postgres_conn.connect() as conn:
+            conn.execute(delete_query)
+        logger.info(
+            f"Registros no PostgreSQL para '{table_name}' deletados com sucesso a partir de {filter_date}."
+        )
+
+        # Inserir os dados do SQL Server no PostgreSQL
+        try:
+            df.to_sql(
+                table_name,
+                postgres_conn,
+                schema="raw_sankhya",
+                if_exists="append",
+                index=False,
+            )
+            logger.info(
+                f"Dados sincronizados com sucesso na tabela 'raw_sankhya.{table_name}'."
+            )
+        except Exception as e:
+            logger.error(f"Erro ao inserir dados no PostgreSQL: {e}")
+
+    except Exception as e:
+        logger.error(f"Erro ao sincronizar a tabela '{table_name}': {e}")
+    finally:
+        del df
+        gc.collect()
+
+
+def migrate_multiple_tables():
+    try:
+        with open("app/databases.json", "r") as file:
+            tables_to_migrate = json.load(file)
+        # Configurar conexões
+        sqlserver_connection = get_connection()
+        postgres_connection = get_postgres_connection()
+        postgres_connection_url = get_postgres_engine_string_url()
+
+        # Iterar sobre a lista de tabelas e processar cada uma
+        for table in tables_to_migrate:
+            table_name = table["table_name"]
+            date_column = table["date_column"]
+            date_filter = table["date_filter"]
+
+            logger.info(f"Iniciando migração para a tabela: {table_name}")
+
+            # Comparar colunas entre SQL Server e PostgreSQL
+            if compare_columns_between_databases(
+                sqlserver_conn=sqlserver_connection,
+                postgres_conn=postgres_connection,
+                table_name=table_name,
+            ):
+                migrate_data(
+                    sqlserver_connection,
+                    postgres_connection_url,
+                    table_name,
+                    date_filter,
+                    date_column,
+                )
+            else:
+                logger.warning(
+                    f"Tabela {table_name} não foi migrada devido a diferenças de colunas."
+                )
+
+    except Exception as e:
+        logger.error(f"Erro durante o processo de migração: {e}")
+    finally:
+        sqlserver_connection.close()
+        postgres_connection.close()
+
+
+def check_and_update_recent_date(table_name, days, date_column):
+    try:
+
+        sqlserver_connection = get_connection()
+        postgres_connection = get_postgres_connection()
+        postgres_connection_url = get_postgres_engine_string_url()
+
+        if compare_columns_between_databases(
+            sqlserver_conn=sqlserver_connection,
+            postgres_conn=postgres_connection,
+            table_name=table_name,
+        ):
+            update_recent_data(
+                sql_conn=sqlserver_connection,
+                postgres_conn=postgres_connection_url,
+                table_name=table_name,
+                date_column_name=date_column,
+                days=days,
+            )
+        else:
+            logger.warning(
+                f"Tabela {table_name} não foi migrada devido a diferenças de colunas."
+            )
+
+    except Exception as e:
+        logger.error(f"Erro durante o processo de migração: {e}")
+    finally:
+        sqlserver_connection.close()
+        postgres_connection.close()
--- app/main.py
+++ app/main.py
@@ -1,59 +1,8 @@
-from db.sqlserver.config import get_connection, get_connection_string_url
-from db.postgres.config import (
-    get_postgres_connection,
-    get_postgres_engine_string_url,
-)
-from db.migrate import compare_columns_between_databases, migrate_data
-import logging
-import json
-from logs.log_config import setup_logging
-
-setup_logging()
-logger = logging.getLogger(__name__)
+from db.migrate import migrate_multiple_tables, check_and_update_recent_date
 
 
 def main():
-    try:
-        with open("app/databases.json", "r") as file:
-            tables_to_migrate = json.load(file)
-        # Configurar conexões
-        sqlserver_connection = get_connection()
-        postgres_connection = get_postgres_connection()
-        postgres_connection_url = get_postgres_engine_string_url()
-
-        # Iterar sobre a lista de tabelas e processar cada uma
-        for table in tables_to_migrate:
-            table_name = table["table_name"]
-            date_column = table["date_column"]
-            date_filter = table["date_filter"]
-
-            logger.info(f"Iniciando migração para a tabela: {table_name}")
-
-            # Comparar colunas entre SQL Server e PostgreSQL
-            if compare_columns_between_databases(
-                sqlserver_conn=sqlserver_connection,
-                postgres_conn=postgres_connection,
-                table_name=table_name,
-            ):
-                # Migrar dados se as colunas forem compatíveis
-                migrate_data(
-                    sqlserver_connection,
-                    postgres_connection_url,
-                    table_name,
-                    date_filter,
-                    date_column,
-                )
-            else:
-                logger.warning(
-                    f"Tabela {table_name} não foi migrada devido a diferenças de colunas."
-                )
-
-    except Exception as e:
-        logger.error(f"Erro durante o processo de migração: {e}")
-    finally:
-        # Fechar conexões
-        sqlserver_connection.close()
-        postgres_connection.close()
+    check_and_update_recent_date(days=10, table_name="tgfite", date_column="dtalter")
 
 
 if __name__ == "__main__":
