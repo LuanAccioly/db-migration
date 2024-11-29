e9da2e86002771c562930465817b8b29cafaf83e
Luan Accioly <acciolyluan@gmail.com>
2024-11-29 15:58:37 -0300

    fix: migração múltipla na main

--- app/main.py
+++ app/main.py
@@ -1,14 +1,8 @@
 from db.migrate import migrate_multiple_tables, check_and_update_recent_date
-from db.postgres.utils import check_table_exists_postgres, postgres_check_table_columns
-from db.postgres.config import get_postgres_connection
 
 
 def main():
-    engine = get_postgres_connection()
-    conn = engine.cursor()
-    # check_and_update_recent_date(days=10, table_name="tgfcot", date_column="dtalter")
-    # check_table_exists_postgres("tcbint")
-    print(postgres_check_table_columns(conn, "tcbint"))
+    migrate_multiple_tables()
 
 
 if __name__ == "__main__":
