0f889f0defbe4cd1e878f9ccdf8e6e03d07215ef
Luan Accioly <acciolyluan@gmail.com>
2024-12-03 11:25:42 -0300

    feat: cria função de verificar chaves primárias de uma tabela

--- app/db/sqlserver/utils.py
+++ app/db/sqlserver/utils.py
@@ -45,3 +45,26 @@ def sqlserver_check_table_columns(sqlserver_conn, table_name):
     except Exception as e:
         logger.error(f"Erro ao verificar colunas da tabela '{table_name}': {e}")
         raise
+
+
+def sqlserver_check_primary_keys(sqlserver_conn, table_name):
+    query = f"""
+    SELECT 
+        KCU.COLUMN_NAME AS ColumnName
+    FROM 
+        INFORMATION_SCHEMA.TABLE_CONSTRAINTS AS TC
+    INNER JOIN
+        INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS KCU
+        ON TC.CONSTRAINT_NAME = KCU.CONSTRAINT_NAME
+    WHERE 
+        TC.TABLE_NAME = '{table_name}' 
+        AND TC.TABLE_SCHEMA = 'sankhya' 
+        AND TC.CONSTRAINT_TYPE = 'PRIMARY KEY';
+    """
+    try:
+        columns = [row[0] for row in sqlserver_conn.execute(query).fetchall()]
+        pk_columns = [col.lower() for col in columns]
+        return pk_columns
+    except Exception as e:
+        logger.error(f"Erro ao verificar PK's da tabela '{table_name}': {e}")
+        raise
