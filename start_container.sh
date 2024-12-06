d03e4e1f7f75223b4a734f423c256fc7b68f87d3
Luan Accioly <acciolyluan@gmail.com>
2024-12-06 17:29:18 -0300

    fix: exit code

--- start_container.sh
+++ start_container.sh
@@ -6,6 +6,8 @@ docker run -d --name natto-byte-api -p "2712:2712" natto-byte api
 # Verifica se o container foi iniciado com sucesso
 if [ $? -eq 0 ]; then
     echo "Container natto-byte-api iniciado com sucesso."
+    exit 0  # Sucesso
 else
     echo "Falha ao iniciar o container."
+    exit 1  # Erro, o Airflow vai capturar isso
 fi
