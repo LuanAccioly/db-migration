b7c14f3fbbf9ddd3e31044951536e4dac1ab9d2e
Luan Accioly <acciolyluan@gmail.com>
2024-12-06 17:37:24 -0300

    feat: verifica se container já existe

--- start_container.sh
+++ start_container.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+# Verifica se o container natto-byte-api já existe
+if docker ps -a --format '{{.Names}}' | grep -q "natto-byte-api"; then
+    echo "Container natto-byte-api já existe. Parando e removendo..."
+    
+    # Para o container, caso esteja em execução
+    docker stop natto-byte-api
+    
+    # Remove o container
+    docker rm natto-byte-api
+fi
+
 # Executa o Docker container
-docker run -d --name natto-byte-api -p "2712:2712" natto-byte
+docker run -d --name natto-byte-api -p "2712:2712" natto-byte api
 
 # Verifica se o container foi iniciado com sucesso
 if [ $? -eq 0 ]; then
